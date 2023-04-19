1d212b6b7af3ae2871b792d4d4b252d009968685
kaseyuu <kaseywu130@gmail.com>
2023-04-19 14:07:43 +1000

    Set up Express static files folder

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+var express = require("express")
+var app = express()
+var port = process.env.port || 3000;
+
+app.use(express.static(__dirname + '/public'))
+
+app.listen(port, () => {
+    console.log("App listening to: " + port)
+})
+
 console.log("Hello World")
