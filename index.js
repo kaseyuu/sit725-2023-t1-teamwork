26219be474576372c86cb59eb27e41d26a904635
kaseyuu <kaseywu130@gmail.com>
2023-05-28 23:33:52 +1000

    Search result page backend setup
    I have used an express template engine called handlebars to  implement the search result page. A views folder was created and the search results were being displayed in a hardcoded way.

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-var express = require("express")
-var app = express()
+var express = require("express");
+var { engine } = require("express-handlebars");
+
+var app = express();
 var port = process.env.port || 3000;
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+app.get("/clothes", (req, res) => {
+    // TODO: Get items from DB dynamitaclly
+    const items = [
+        {
+            imageUrl: "./images/dress1.png",
+            name: "mini dress in monochrome print",
+            price: 60,
+        },
+        {
+            imageUrl: "./images/dress2.png",
+            name: "Beautiful Zara Girl white dress size 9 / 10",
+            price: 15,
+        },
+        {
+            imageUrl: "./images/dress3.png",
+            name: "Black mini dress with floral design",
+            price: 30,
+        },
+        {
+            imageUrl: "./images/shoe1.png",
+            name: "Nearly New Wayne Cooper High Heels",
+            price: 29,
+        },
+    ];
+    res.render("clothes", { items });
+});
 
 app.listen(port, () => {
-    console.log("App listening to: " + port)
-})
+    console.log("App listening to: " + port);
+});
 
-console.log("Hello World")
+console.log("Hello World");
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,10 @@
 const searchButton = document.getElementById('searchButton');
 
-searchButton.addEventListener('click', function (event)){
+searchButton.addEventListener('click', function (event) {
     event.preventDefault();
     const searchInput = document.getElementById('searchInput').value;
     performSearch(searchInput);
-};
+});
 
 function performSearch(query) {
     console.log('search query:', query);
