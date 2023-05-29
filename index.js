1a1800045aa496097bdd6d9fcc1f754954b25d4a
yonndenila97 <s223572679@deakin.edu.au>
2023-05-29 14:42:24 +1000

    commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var express = require("express")
-var app = express()
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + 'public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 var port = process.env.port || 3000;
-
-app.use(express.static(__dirname + '/public'))
-
 app.listen(port, () => {
     console.log("App listening to: " + port)
 })
 
-console.log("Hello World")
+console.log("Hello World");
